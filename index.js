19264870fb6acef9ef8b2aed1766b989c22d6d36
André Luiz <silva.ala82@gmail.com>
2023-08-25 21:23:35 -0300

    Refactor: Update and change logic for SQL query consult at repository comment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const userController = require('./modules/users/controller.user');
 const loginController = require('./modules/auth/controller.auth');
 const agendaController = require('./modules/agenda/controller.agenda');
+const commentController = require('./modules/comment/controller.coment');
 server.use(express.urlencoded({extended:true}))
 server.use(express.json());
 const port = process.env.SERVER_PORT
@@ -29,6 +30,7 @@ server.use(cors({ "origin": "*",
 server.use('/users', userController);
 server.use('/auth', loginController);
 server.use('/agenda', agendaController);
+server.use('/comment', commentController);
 
 server.listen(port, () =>
 console.log('Server rodando na porta', port));
---
+++ modules/comment/controller.coment.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { authorization } = require('../auth/middleware.auth');
+const validateMessagePost = require('../agenda/middleware.agenda');
+const { createNewAgendaComment, readAgendaComment } = require('./services.coment');
+const router = express.Router();
+
+
+router.post('/', authorization, validateMessagePost, async(req, res) => {
+    try {
+        const ownerIdComment = req.userId
+        const createdNewComment = await createNewAgendaComment(ownerIdComment, req.body)
+        res.status(201).json({data:createdNewComment, message:"Comentário realizado com sucesso"})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+router.patch('/read/:id?', authorization, async(req, res) => {
+    try{
+        const ownerIdcomment = req.userId
+        const idPost = req.params.id
+        const readComment = await readAgendaComment(ownerIdcomment, idPost)
+        res.status(201).json({data:readComment, message:"Comentário marcado com sucesso"})
+    } catch (error){
+        console.log(error)
+        res.status(500).json({message:error.message})
+    }
+})
+
+module.exports = router
