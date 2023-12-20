b31c2f07595832a733f1f432af4b36a983c4ef90
André Luiz <silva.ala82@gmail.com>
2023-12-19 21:48:25 -0300

    fix: corrects parameters id at edit users route

--- modules/users/controller.user.js
+++ modules/users/controller.user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
 const multer = require('../../config/multer')
-const { gettAllUsers, 
-        createNewUser,
-        updateUser,
-        deleteUser,
-        validateName} = require('./service.user');
+const { gettAllUsers,
+    createNewUser,
+    updateUser,
+    deleteUser,
+    validateName } = require('./service.user');
 const { findUserById } = require('./repository.user');
 const { authorization } = require('../auth/middleware.auth');
 
@@ -14,41 +14,41 @@ userRouter.get('/', async (req, res) => {
     res.send({ data: responseAllUsers });
 });
 
-userRouter.get('/:id', authorization, async (req,res) => {
+userRouter.get('/:id', authorization, async (req, res) => {
     const responseUserById = await findUserById(req.params.id)
     res.send({ data: responseUserById });
 });
-  
+
 userRouter.post('/', authorization, validateName, async (req, res) => {
     const newInstanceUser = await createNewUser(req.body);
-    res.status(201).json({ data:newInstanceUser, message: "Usuário criado com sucesso!"})
+    res.status(201).json({ data: newInstanceUser, message: "Usuário criado com sucesso!" })
 });
-  
-userRouter.put('/', authorization, async (req, res) => {
+
+userRouter.put('/:id?', authorization, async (req, res) => {
     try {
         const idUserUpdate = req.params.id
         const userUpdated = await updateUser(idUserUpdate, req.body)
-        res.status(201).json({data:userUpdated, message: "Usuário editado com sucesso!"})
+        res.status(201).json({ data: userUpdated, message: "Usuário editado com sucesso!" })
     } catch (error) {
-        console.log(error.message)  
-        res.status(500).json({  message: "Ocorreu um problema de servidor!"})
-    } 
+        console.log(error.message)
+        res.status(500).json({ message: "Ocorreu um problema de servidor!" })
+    }
 });
 
-userRouter.put('/v2', multer.single('profile_photo'), authorization, async (req, res) => {
+userRouter.put('/v2/:id?', multer.single('profile_photo'), authorization, async (req, res) => {
     try {
-        const userUpdated = await updateUser(req.userId, {...req.body}, req.file)
-        res.status(201).json({data:userUpdated, message: "Usuário editado com sucesso!"})
+        const userUpdated = await updateUser(req.userId, { ...req.body }, req.file)
+        res.status(201).json({ data: userUpdated, message: "Usuário editado com sucesso!" })
     } catch (error) {
-        console.log(error)  
-        res.status(500).json({  message: "Ocorreu um problema de servidor!"})
-    } 
+        console.log(error)
+        res.status(500).json({ message: "Ocorreu um problema de servidor!" })
+    }
 });
-  
+
 userRouter.delete('/:id?', authorization, (req, res) => {
     const UserDeleted = deleteUser(req.params.id)
-    res.json({success:UserDeleted})
+    res.json({ success: UserDeleted })
 });
-  
+
 
 module.exports = userRouter;
